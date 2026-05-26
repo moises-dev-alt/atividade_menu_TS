4aa8d8725fcde7f0facbe7dc9c18d2ef950b3249
moises-dev-alt <moisesgustavo706@gmail.com>
2026-05-26 08:21:46 -0300

    Update exercicio14.ts

--- src/exercicio14.ts
+++ src/exercicio14.ts
@@ -5,6 +5,10 @@ Professor: José Carlos Flores
 Turma: ADS3A
 Componentes:
  25014114-2 - Gustavo Moisés Felipe
+ 25200599-2 - Matheus Henrique Onisanti de Farias
+ 25165638-2 - Luka Anders
+ 25001880-2 - Vitor de Lima Ortiz
+            - Eduardo
 Data: 26 de Maio de 2026
 Descritivo: Escreva um programa que solicite um número inteiro e exiba sua tabuada de 1 a 10
 *******************************************************************************/
