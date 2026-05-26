df660f5ddc86ccfb23d20bc707918ef7b07f8b2c
moises-dev-alt <moisesgustavo706@gmail.com>
2026-05-26 08:16:37 -0300

    corrigind cabeçalho

--- src/exercicio01.ts
+++ src/exercicio01.ts
@@ -6,6 +6,10 @@ Professor: José Carlos Flores
 Turma: ADS3A
 Componentes:
  25014114-2 - Gustavo Moisés Felipe
+ 25200599-2 - Matheus Henrique Onisanti de Farias
+ 25165638-2 - Luka Anders
+ 25001880-2 - Vitor de Lima Ortiz
+            - Eduardo
 Data: 26 de Maio de 2026
 Descritivo: Escreva um programa que receba dois números inteiros e exiba a soma deles
 *******************************************************************************/
