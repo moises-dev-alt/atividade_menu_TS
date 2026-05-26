d44ccb723d7d91172d871bf006fd1626f8c8e6ae
moises-dev-alt <moisesgustavo706@gmail.com>
2026-05-26 08:18:15 -0300

    cabeçalho

--- src/exercicio06.ts
+++ src/exercicio06.ts
@@ -5,6 +5,11 @@ Professor: José Carlos Flores
 Turma: ADS3A
 Componentes:
  25014114-2 - Gustavo Moisés Felipe
+ 25014114-2 - Gustavo Moisés Felipe
+ 25200599-2 - Matheus Henrique Onisanti de Farias
+ 25165638-2 - Luka Anders
+ 25001880-2 - Vitor de Lima Ortiz
+            - Eduardo
 Data: 26 de Maio de 2026
 Descritivo: Escreva um programa que leia 5 números do usuário e os armazene em um array, depois
 exiba todos os números
